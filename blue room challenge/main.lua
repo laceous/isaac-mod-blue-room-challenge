e66ea583b3c6505fb5951549f3c6d019a368aca0
laceous <laceous@gmail.com>
2022-01-23 20:17:15 -0800

    add curses

--- blue room challenge/main.lua
+++ blue room challenge/main.lua
@@ -1,20 +1,40 @@
-local mod = RegisterMod('Blue Room Challenge', 1)
+local mod = RegisterMod('Blue Room Challenge (+Curses)', 1)
 local json = require('json')
 local game = Game()
 
 mod.blueRoomIndex = nil
 mod.onGameStartHasRun = false
+mod.curseOfBlueRooms = 'Curse of Blue Rooms!'
+mod.curseOfBlueRooms2 = 'Curse of Blue Rooms!!'
+mod.curseOfPitchBlack = 'Curse of Pitch Black!'
+-- 1 << x is the same as 2 ^ x except that the first returns an integer and the second returns a float
+mod.flagCurseOfBlueRooms = 1 << Isaac.GetCurseIdByName(mod.curseOfBlueRooms) - 1
+mod.flagCurseOfBlueRooms2 = 1 << Isaac.GetCurseIdByName(mod.curseOfBlueRooms2) - 1
+mod.flagCurseOfPitchBlack = 1 << Isaac.GetCurseIdByName(mod.curseOfPitchBlack) - 1
+mod.rng = RNG()
+
+mod.difficulty = {
+  [Difficulty.DIFFICULTY_NORMAL] = 'normal',
+  [Difficulty.DIFFICULTY_HARD] = 'hard',
+  [Difficulty.DIFFICULTY_GREED] = 'greed',
+  [Difficulty.DIFFICULTY_GREEDIER] = 'greedier'
+}
 
 mod.state = {}
 mod.state.blueRooms = {}                    -- clear state for blue rooms
 mod.state.leaveDoor = DoorSlot.NO_DOOR_SLOT -- bug fix: the game doesn't remember LeaveDoor on continue
 mod.state.stageSeed = nil
+mod.state.probabilityBlueRooms =  { normal = 3, hard = 20, greed = 0, greedier = 0 }
+mod.state.probabilityBlueRooms2 = { normal = 0, hard = 3,  greed = 0, greedier = 0 }
+mod.state.probabilityPitchBlack = { normal = 0, hard = 3,  greed = 0, greedier = 0 }
+mod.state.overrideCurses = false
 
 function mod:onGameStart(isContinue)
   local level = game:GetLevel()
   local seeds = game:GetSeeds()
   local stageSeed = seeds:GetStageSeed(level:GetStage())
   mod.state.stageSeed = stageSeed
+  mod:seedRng()
   
   if mod:HasData() then
     local _, state = pcall(json.decode, mod:LoadData())
@@ -31,6 +51,18 @@ function mod:onGameStart(isContinue)
           end
         end
       end
+      for _, probability in ipairs({ 'probabilityBlueRooms', 'probabilityBlueRooms2', 'probabilityPitchBlack' }) do
+        if type(state[probability]) == 'table' then
+          for _, difficulty in ipairs({ 'normal', 'hard', 'greed', 'greedier' }) do
+            if math.type(state[probability][difficulty]) == 'integer' and state[probability][difficulty] >= 0 and state[probability][difficulty] <= 100 then
+              mod.state[probability][difficulty] = state[probability][difficulty]
+            end
+          end
+        end
+      end
+      if type(state.overrideCurses) == 'boolean' then
+        mod.state.overrideCurses = state.overrideCurses
+      end
     end
     
     if not isContinue then
@@ -39,23 +71,22 @@ function mod:onGameStart(isContinue)
     end
   end
   
-  if not mod:isChallenge() then
-    return
+  if mod:hasCurse(mod.flagCurseOfBlueRooms | mod.flagCurseOfBlueRooms2) or mod:isChallenge() then
+    if mod:isBlueRoom(level:GetCurrentRoomDesc()) then
+      mod:setBlueRoomIndex()
+      mod:setBlueRoomState()
+    end
   end
   
-  if mod:isBlueRoom(level:GetCurrentRoomDesc()) then
-    mod:setBlueRoomIndex()
-    mod:setBlueRoomState()
-  end
-  
-  -- spawn random boss pool item and book on start
-  if not isContinue then
-    local itemPool = game:GetItemPool()
-    local collectible = itemPool:GetCollectible(ItemPoolType.POOL_BOSS, false, Random(), CollectibleType.COLLECTIBLE_NULL)
-    Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, collectible, Vector(280, 280), Vector(0,0), nil) -- game:Spawn
-    
-    local book = mod:isDarkChallenge() and CollectibleType.COLLECTIBLE_SATANIC_BIBLE or CollectibleType.COLLECTIBLE_BOOK_OF_REVELATIONS
-    Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, book, Vector(360, 280), Vector(0,0), nil)
+  if mod:isChallenge() then
+    if not isContinue then -- spawn random boss pool item and book on start
+      local itemPool = game:GetItemPool()
+      local collectible = itemPool:GetCollectible(ItemPoolType.POOL_BOSS, false, Random(), CollectibleType.COLLECTIBLE_NULL)
+      Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, collectible, Vector(280, 280), Vector(0,0), nil) -- game:Spawn
+      
+      local book = mod:isDarkChallenge() and CollectibleType.COLLECTIBLE_SATANIC_BIBLE or CollectibleType.COLLECTIBLE_BOOK_OF_REVELATIONS
+      Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, book, Vector(360, 280), Vector(0,0), nil)
+    end
   end
   
   mod.onGameStartHasRun = true
@@ -63,6 +94,29 @@ end
 
 function mod:onGameExit()
   mod:SaveData(json.encode(mod.state))
+end
+
+function mod:onCurseEval(curses)
+  if mod:isChallenge() then
+    local flag = mod:isCursedChallenge() and mod.flagCurseOfBlueRooms2 or mod.flagCurseOfBlueRooms
+    if mod:isDarkChallenge() then
+      flag = flag | mod.flagCurseOfPitchBlack
+    end
+    
+    return curses | flag
+  end
+  
+  if curses == LevelCurse.CURSE_NONE or mod.state.overrideCurses then
+    if mod.rng:RandomInt(100) < mod.state.probabilityBlueRooms[mod.difficulty[game.Difficulty]] then
+      return mod.flagCurseOfBlueRooms
+    elseif mod.rng:RandomInt(100) < mod.state.probabilityBlueRooms2[mod.difficulty[game.Difficulty]] then
+      return mod.flagCurseOfBlueRooms2
+    elseif mod.rng:RandomInt(100) < mod.state.probabilityPitchBlack[mod.difficulty[game.Difficulty]] then
+      return mod.flagCurseOfPitchBlack
+    end
+  end
+  
+  return curses
 end
 
 function mod:onNewLevel()
@@ -75,64 +129,56 @@ end
 
 -- onNewRoom doesn't enable FLAG_CURSED_MIST quickly enough
 function mod:onPreNewRoom()
-  if not mod:isCursedChallenge() then
-    return
-  end
-  
-  local level = game:GetLevel()
-  local roomDesc = level:GetRoomByIdx(level:GetCurrentRoomIndex(), -1) -- writeable
-  
-  if mod:isBlueRoom(roomDesc) then
-    roomDesc.Flags = roomDesc.Flags | RoomDescriptor.FLAG_CURSED_MIST
+  if mod:hasCurse(mod.flagCurseOfBlueRooms2) or mod:isCursedChallenge() then
+    local level = game:GetLevel()
+    local roomDesc = level:GetRoomByIdx(level:GetCurrentRoomIndex(), -1) -- writeable
+    
+    if mod:isBlueRoom(roomDesc) then
+      roomDesc.Flags = roomDesc.Flags | RoomDescriptor.FLAG_CURSED_MIST
+    end
   end
 end
 
 function mod:onNewRoom()
-  if not mod:isChallenge() then
-    return
-  end
-  
   local level = game:GetLevel()
   local roomDesc = level:GetRoomByIdx(level:GetCurrentRoomIndex(), -1)
   
   mod.state.leaveDoor = level.LeaveDoor
   
-  -- this needs to happen in onGameStart the first time (which happens after onNewRoom)
-  if mod.onGameStartHasRun then
-    if mod:isBlueRoom(roomDesc) then
-      mod:setBlueRoomIndex() -- calculate this once
-      mod:setBlueRoomState()
+  if mod:hasCurse(mod.flagCurseOfBlueRooms | mod.flagCurseOfBlueRooms2) or mod:isChallenge() then
+    -- this needs to happen in onGameStart the first time (which happens after onNewRoom)
+    if mod.onGameStartHasRun then
+      if mod:isBlueRoom(roomDesc) then
+        mod:setBlueRoomIndex() -- calculate this once
+        mod:setBlueRoomState()
+      end
+    end
+    
+    -- set blue room redirect on surrounding rooms
+    -- this only works if we're on the grid, otherwise the other end of the blue room might not have a door
+    if roomDesc.GridIndex >= 0 and not mod:isMinesEscapeSequence() then
+      mod:setBlueRoomRedirects(mod:getSurroundingGridIndexes(roomDesc))
     end
   end
   
-  -- set blue room redirect on surrounding rooms
-  -- this only works if we're on the grid, otherwise the other end of the blue room might not have a door
-  if roomDesc.GridIndex >= 0 then
-    mod:setBlueRoomRedirects(mod:getSurroundingGridIndexes(roomDesc))
+  if mod:hasCurse(mod.flagCurseOfPitchBlack) or mod:isDarkChallenge() then
+    roomDesc.Flags = roomDesc.Flags | RoomDescriptor.FLAG_PITCH_BLACK
   end
-  
-  if not mod:isDarkChallenge() then
-    return
-  end
-  
-  roomDesc.Flags = roomDesc.Flags | RoomDescriptor.FLAG_PITCH_BLACK
 end
 
 function mod:onUpdate()
-  if not mod:isChallenge() then
-    return
-  end
-  
-  local level = game:GetLevel()
-  local roomDesc = level:GetCurrentRoomDesc() -- read-only
-  
-  if mod:isBlueRoom(roomDesc) then
-    mod:setBlueRoomState()
-  end
-  
-  -- this is here because red rooms could be created at any time
-  if roomDesc.GridIndex >= 0 then
-    mod:setBlueRoomRedirects(mod:getSurroundingGridIndexes(roomDesc))
+  if mod:hasCurse(mod.flagCurseOfBlueRooms | mod.flagCurseOfBlueRooms2) or mod:isChallenge() then
+    local level = game:GetLevel()
+    local roomDesc = level:GetCurrentRoomDesc() -- read-only
+    
+    if mod:isBlueRoom(roomDesc) then
+      mod:setBlueRoomState()
+    end
+    
+    -- this is here because red rooms could be created at any time
+    if roomDesc.GridIndex >= 0 and not mod:isMinesEscapeSequence() then
+      mod:setBlueRoomRedirects(mod:getSurroundingGridIndexes(roomDesc))
+    end
   end
 end
 
@@ -278,17 +324,55 @@ function mod:mergeIndexes(index1, index2)
   return low .. '-' .. high
 end
 
+function mod:seedRng()
+  repeat
+    local rand = Random()  -- 0 to 2^32
+    if rand > 0 then       -- if this is 0, it causes a crash later on
+      mod.rng:SetSeed(rand, 1)
+    end
+  until(rand > 0)
+end
+
+function mod:isMinesEscapeSequence()
+  local level = game:GetLevel()
+  local stage = level:GetStage()
+  local stageType = level:GetStageType()
+  local roomDesc = level:GetCurrentRoomDesc()
+  
+  return stage == LevelStage.STAGE2_2 and (stageType == StageType.STAGETYPE_REPENTANCE or stageType == StageType.STAGETYPE_REPENTANCE_B) and mod:getDimension(roomDesc) == 1
+end
+
+function mod:getDimension(roomDesc)
+  local level = game:GetLevel()
+  local ptrHash = GetPtrHash(roomDesc)
+  
+  -- 0: main dimension
+  -- 1: secondary dimension, used by downpour mirror dimension and mines escape sequence
+  -- 2: death certificate dimension
+  for i = 0, 2 do
+    if ptrHash == GetPtrHash(level:GetRoomByIdx(roomDesc.SafeGridIndex, i)) then
+      return i
+    end
+  end
+  return -1
+end
+
+function mod:hasCurse(curse)
+  local level = game:GetLevel()
+  local curses = level:GetCurses()
+  
+  return mod:hasAnyFlag(curses, curse)
+end
+
+function mod:hasAnyFlag(flags, flag)
+  return flags & flag ~= 0
+end
+
 function mod:isChallenge()
   local challenge = Isaac.GetChallenge()
   return challenge == Isaac.GetChallengeIdByName('Blue Room Challenge') or
          challenge == Isaac.GetChallengeIdByName('Blue Room Challenge (Cursed)') or
          challenge == Isaac.GetChallengeIdByName('Dark Room Challenge') or
-         challenge == Isaac.GetChallengeIdByName('Dark Room Challenge (Cursed)')
-end
-
-function mod:isDarkChallenge()
-  local challenge = Isaac.GetChallenge()
-  return challenge == Isaac.GetChallengeIdByName('Dark Room Challenge') or
          challenge == Isaac.GetChallengeIdByName('Dark Room Challenge (Cursed)')
 end
 
@@ -298,9 +382,76 @@ function mod:isCursedChallenge()
          challenge == Isaac.GetChallengeIdByName('Dark Room Challenge (Cursed)')
 end
 
+function mod:isDarkChallenge()
+  local challenge = Isaac.GetChallenge()
+  return challenge == Isaac.GetChallengeIdByName('Dark Room Challenge') or
+         challenge == Isaac.GetChallengeIdByName('Dark Room Challenge (Cursed)')
+end
+
+-- start ModConfigMenu --
+function mod:setupModConfigMenu()
+  ModConfigMenu.AddSetting(
+    mod.Name,
+    'Curses',
+    {
+      Type = ModConfigMenu.OptionType.BOOLEAN,
+      CurrentSetting = function()
+        return mod.state.overrideCurses
+      end,
+      Display = function()
+        return (mod.state.overrideCurses and 'Override' or 'Respect') .. ' curses'
+      end,
+      OnChange = function(b)
+        mod.state.overrideCurses = b
+      end,
+      Info = { 'The game may have already set a curse', 'Should we respect it or potentially override it?' }
+    }
+  )
+  ModConfigMenu.AddSpace(mod.Name, 'Curses')
+  for i, probability in ipairs({ 'probabilityBlueRooms', 'probabilityBlueRooms2', 'probabilityPitchBlack' }) do
+    if i == 1 then
+      ModConfigMenu.AddTitle(mod.Name, 'Curses', mod.curseOfBlueRooms)
+    elseif i == 2 then
+      ModConfigMenu.AddTitle(mod.Name, 'Curses', mod.curseOfBlueRooms2)
+    else -- 3
+      ModConfigMenu.AddTitle(mod.Name, 'Curses', mod.curseOfPitchBlack)
+    end
+    for _, difficulty in ipairs({ 'normal', 'hard', 'greed', 'greedier' }) do
+      ModConfigMenu.AddSetting(
+        mod.Name,
+        'Curses',
+        {
+          Type = ModConfigMenu.OptionType.NUMBER,
+          CurrentSetting = function()
+            return mod.state[probability][difficulty]
+          end,
+          Minimum = 0,
+          Maximum = 100,
+          Display = function()
+            return difficulty .. ': ' .. mod.state[probability][difficulty] .. '%'
+          end,
+          OnChange = function(n)
+            mod.state[probability][difficulty] = n
+          end,
+          Info = { 'The curses here are evaluated in order' }
+        }
+      )
+    end
+    if i ~= 3 then
+      ModConfigMenu.AddSpace(mod.Name, 'Curses')
+    end
+  end
+end
+-- end ModConfigMenu --
+
 mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, mod.onGameStart)
 mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, mod.onGameExit)
+mod:AddCallback(ModCallbacks.MC_POST_CURSE_EVAL, mod.onCurseEval)
 mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, mod.onNewLevel)
 mod:AddCallback(ModCallbacks.MC_PRE_ROOM_ENTITY_SPAWN, mod.onPreNewRoom) -- fires 0-n times per new room, 1-n for blue rooms
 mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.onNewRoom)
 mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.onUpdate)
+
+if ModConfigMenu then
+  mod:setupModConfigMenu()
+end
